9f2b5ffb3302d9a15b60da3613aa840b7b23130f
Dammika-Pushpakumara <ivdammika@gmail.com>
2024-03-24 16:31:21 +0530

    login and signup compnent designed.

---
+++ src/app/page/register/register.component.ts
@@ -0,0 +1,43 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [HttpClientModule, FormsModule, CommonModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent implements OnInit {
+
+  private http;
+  public countryList:any;
+  public selectedCountry:any;
+  public selectedCountryCode:any;
+
+  constructor(private httpClient:HttpClient){
+    this.http=httpClient;
+  }
+
+  ngOnInit(): void {
+    this.loadCountries();
+  }
+
+  loadCountries(){
+    let api ="https://restcountries.com/v3.1/all";
+    this.http.get(api).subscribe(res =>{
+      this.countryList=res;
+      console.log(res);
+    })
+  }
+  setSelectedCountry(country:any){
+    this.selectedCountry=country;
+    this.selectedCountryCode=country.idd.root+""+country.idd.suffixes[0];
+    console.log(this.selectedCountryCode);
+    console.log(this.selectedCountry);
+  }
+}
